3c21320682914d415a806911dbcb45b059e7d6f9
dima-kulikov <skaizy44@gmail.com>
2020-09-24 09:49:46 +0300

    bild
    bild

---
+++ build/js/scripts.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// Функция смены цвета каркаса авто
+function CarColor() {
+  car__frame.style.color = '#e6ff02';
+}
+
+function CarColor2() {
+  car__frame.style.color = '#ff0800';
+}
+
+function CarColor3() {
+  car__frame.style.color = '#001aff';
+}
+
+function CarColor4() {
+  car__frame.style.color = '#2dbd4c';
+}
+
+avto_color.addEventListener('click', CarColor);
+avto_color2.addEventListener('click', CarColor2);
+avto_color3.addEventListener('click', CarColor3);
+avto_color4.addEventListener('click', CarColor4); // ЗАМЕНА ДИСКОВ У МАШИНЫ
+
+function ColorWheel() {
+  radius.style.backgroundColor = '#e6ff02';
+}
+
+avto_wheel1.addEventListener('click', ColorWheel);
