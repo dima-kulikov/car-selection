b850384f04fd843ce736d50fc4549e0a76922c82
dima-kulikov <skaizy44@gmail.com>
2020-09-24 22:40:12 +0300

    24.09.2020
    24.09.2020

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,21 +1,22 @@
 "use strict";
 
 // Функция смены цвета каркаса авто
-// const car__frame = document.querySelector('car__frame');
+var car__frame = document.querySelector('#car__frame');
+
 function CarColor() {
-  car__frame.style.color = '#e6ff02';
+  car__frame.style.color = '#bdc023';
 }
 
 function CarColor2() {
-  car__frame.style.color = '#ff0800';
+  car__frame.style.color = '#c94831';
 }
 
 function CarColor3() {
-  car__frame.style.color = '#001aff';
+  car__frame.style.color = '#31bfc9';
 }
 
 function CarColor4() {
-  car__frame.style.color = '#2dbd4c';
+  car__frame.style.color = '#317428';
 }
 
 avto_color.addEventListener('click', CarColor);
@@ -23,11 +24,30 @@ avto_color2.addEventListener('click', CarColor2);
 avto_color3.addEventListener('click', CarColor3);
 avto_color4.addEventListener('click', CarColor4); // ЗАМЕНА ДИСКОВ У МАШИНЫ
 
-var radius = document.querySelector('#radius', '#radius1');
+var radius1 = document.querySelector('#radius1');
+var radius2 = document.querySelector('#radius2');
 
 function ColorWheel() {
-  radius.style.backgroundColor = '#e6ff02';
+  radius1.style.backgroundColor = '#bdc023';
+  radius2.style.backgroundColor = '#bdc023';
 }
 
-console.log(radius);
+function ColorWheel2() {
+  radius1.style.backgroundColor = '#c94831';
+  radius2.style.backgroundColor = '#c94831';
+}
+
+function ColorWheel3() {
+  radius1.style.backgroundColor = '#31bfc9';
+  radius2.style.backgroundColor = '#31bfc9';
+}
+
+function ColorWheel4() {
+  radius1.style.backgroundColor = '#317428';
+  radius2.style.backgroundColor = '#317428';
+}
+
 avto_wheel1.addEventListener('click', ColorWheel);
+avto_wheel2.addEventListener('click', ColorWheel2);
+avto_wheel3.addEventListener('click', ColorWheel3);
+avto_wheel4.addEventListener('click', ColorWheel4);
--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
 // Функция смены цвета каркаса авто
-// const car__frame = document.querySelector('car__frame');
+const car__frame = document.querySelector('#car__frame');
 
 function CarColor() {
-  car__frame.style.color = '#e6ff02';
+  car__frame.style.color = '#bdc023';
 }
 function CarColor2() {
-  car__frame.style.color = '#ff0800';
+  car__frame.style.color = '#c94831';
 }
 function CarColor3() {
-  car__frame.style.color = '#001aff';
+  car__frame.style.color = '#31bfc9';
 }
 function CarColor4() {
-  car__frame.style.color = '#2dbd4c';
+  car__frame.style.color = '#317428';
 }
 
 avto_color.addEventListener('click', CarColor);
@@ -20,12 +20,29 @@ avto_color3.addEventListener('click', CarColor3);
 avto_color4.addEventListener('click', CarColor4);
 
 // ЗАМЕНА ДИСКОВ У МАШИНЫ
-const radius = document.querySelector('#radius', '#radius1');
+let radius1 = document.querySelector('#radius1');
+let radius2 = document.querySelector('#radius2');
 
 function ColorWheel() {
-  radius.style.backgroundColor = '#e6ff02';
+  radius1.style.backgroundColor = '#bdc023';
+  radius2.style.backgroundColor = '#bdc023';
+}
+function ColorWheel2() {
+  radius1.style.backgroundColor = '#c94831';
+  radius2.style.backgroundColor = '#c94831';
+}
+function ColorWheel3() {
+  radius1.style.backgroundColor = '#31bfc9';
+  radius2.style.backgroundColor = '#31bfc9';
+}
+function ColorWheel4() {
+  radius1.style.backgroundColor = '#317428';
+  radius2.style.backgroundColor = '#317428';
 }
 
-console.log(radius);
 
 avto_wheel1.addEventListener('click', ColorWheel);
+avto_wheel2.addEventListener('click', ColorWheel2);
+avto_wheel3.addEventListener('click', ColorWheel3);
+avto_wheel4.addEventListener('click', ColorWheel4);
+
